a2f4083acc1b35b997cbafc5d5a38576aa58749b
gleilson2 <gleilson.1@yahoo.com>
2025-07-27 16:09:57 -0300

    Remover seção Tamanhos do Produto

--- src/components/Admin/ProductsPanel.tsx
+++ src/components/Admin/ProductsPanel.tsx
@@ -692,21 +692,6 @@ const ProductsPanel: React.FC = () => {
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-
-      {showModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
-          <div className="bg-white rounded-lg max-w-7xl w-full max-h-[95vh] flex flex-col">
-            <div className="flex justify-between items-center p-6 border-b">
-              <h3 className="text-xl font-semibold">
-                {editingProduct ? 'Editar Produto' : 'Novo Produto'}
-              </h3>
-              <button
-                onClick={() => setShowModal(false)}
-                className="text-gray-500 hover:text-gray-700"
-              >
                 <X className="w-6 h-6" />
               </button>
             </div>
